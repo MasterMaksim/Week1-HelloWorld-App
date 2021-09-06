202a3b3c6a02c320d9cb6b8901993548b0332d58
Maksim Muravjov <mmuravjo@tktk.ee>
2021-09-06 18:45:02 +0300

    Create Hello World Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string MyName;
+            MyName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + MyName);
+
+            Console.Read();
+        }
+    }
+}
